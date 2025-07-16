eae1f97065152761e7be97c5a78ce53b0ecbd6a8
gatolda <impo.rrchile@gmail.com>
2025-07-16 17:32:12 -0400

    Update controlador.py

--- logica/logica/controlador.py
+++ logica/logica/controlador.py
@@ -1,8 +1,10 @@
 def leer_sensores(self):
     """Obtiene y devuelve las lecturas del sensor."""
+    print(f"🛠 DEBUG: Tipo de self.sensor = {type(self.sensor)}")
     print("🛠 DEBUG: Llamando a self.sensor.read()...")
     datos = self.sensor.read()
     print(f"🛠 DEBUG: Resultado de self.sensor.read() = {datos}")
+
     if datos.get("status") != "OK":
         print("❌ Error al leer el sensor dentro de ControladorClima")
         return None
